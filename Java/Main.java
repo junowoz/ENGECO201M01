98e4e39556185dc09011cd41ad911f20a5218678
junowoz <junogouvea@gmail.com>
2023-02-14 11:04:48 -0400

    java classe

---
+++ Java/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Esta forma de programar eleva a programação e o desenvolvimento de sistemas. Aumentando consideravelmente o tempo de desenvolver bem como a performasse.
+    A OO é um mecanismo moderno que ajuda a definir a estrutura de programas baseada nos conceitos do mundo real, sejam eles reais ou abstratos.
+
+    1.ABSTRAÇÃO: 
+        - É o processo através do qual detalhes são ignorados, para nos concentrarmos nas características essenciais.
+    2.OBJETO: 
+        - É uma entidade do mundo real que tem uma identidade. Objetos podem representar entidades concretas ou entidades conceituais.
+        - A estrutura de um objeto é representada por suas  características, a característica de um objeto é representado através de seus atributos.
+        - O comportamento de um objeto é representado pelo conjunto de operações que podem ser executadas, e chamadas de metodos.
+    3.CLASSE:
+        - 
+    4.MENSAGEM:
+    5.ENCAPSULAMENTO:
+    6.HERANÇA:
+    7.POLIMOFISMO:
+
+ */
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
